8b79047ba56bee6a74498dc0b1028c3106db92ed
Lường Đức Mạnh <dtc245200212@ictu.edu.vn>
2026-09-22 21:13:27 +0700

    Create Class and Teacher tables

---
+++ student_management.sql
@@ -0,0 +1,15 @@
+CREATE DATABASE `student-management`;
+
+USE `student-management`;
+
+CREATE TABLE Class(
+    id INT,
+    name VARCHAR(200)
+);
+
+CREATE TABLE Teacher(
+    id INT,
+    name VARCHAR(200),
+    age INT,
+    country VARCHAR(50)
+);
